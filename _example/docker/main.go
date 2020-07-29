8faefa63a4e334975787068d89c0158a470c3db8
Tom Wiesing <tkw01536@gmail.com>
2020-07-29 12:16:18 +0200

    Fix broken example code for docker

--- _example/docker/main.go
+++ _example/docker/main.go
@@ -18,16 +18,16 @@ var logger = log.New(os.Stderr, "", log.LstdFlags)
 func main() {
 	// make the server
 	server := dockerproxy.NewDockerProxyServer(logger, dockerproxy.ServerOptions{
-		Client:              cli,
-		ListenAddress:       ListenAddress,
-		DockerLabelUser:     DockerLabelUser,
-		DockerLabelAuthFile: DockerLabelAuthFile,
-		DockerLabelKey:      "",
-		ContainerShell:      ContainerShell,
-		NoAuth:              NoAuth,
-		IdleTimeout:         time.Duration(IdleTimeout) * time.Second,
-		ForwardPorts:        ForwardPorts,
-		ReversePorts:        ReversePorts,
+		Client:                cli,
+		ListenAddress:         ListenAddress,
+		DockerLabelUser:       DockerLabelUser,
+		DockerLabelAuthFile:   DockerLabelAuthFile,
+		DockerLabelKey:        "",
+		ContainerShell:        ContainerShell,
+		DisableAuthentication: DisableAuthentication,
+		IdleTimeout:           time.Duration(IdleTimeout) * time.Second,
+		ForwardAddresses:      ForwardAddresses,
+		ReverseAddresses:      ReverseAddresses,
 	})
 
 	// load rsa host key
@@ -56,14 +56,14 @@ var (
 	DockerLabelAuthFile = "de.tkw1536.proxyssh.authfile"
 	// ContainerShell is the executable to run within the container
 	ContainerShell = "/bin/sh"
-	// NoAuth disables authentication
-	NoAuth = false
+	// DisableAuthentication disables authentication
+	DisableAuthentication = false
 	// IdleTimeout is the timeout after which an idle connection is killed
 	IdleTimeout = 30
-	// ForwardPorts are ports that are allowed to be forwarded
-	ForwardPorts = utils.NetworkAddressListVar(nil)
-	// ReversePorts are ports that are allowed to be forwarded (in reverse)
-	ReversePorts = utils.NetworkAddressListVar(nil)
+	// ForwardAddresses are ports that are allowed to be forwarded
+	ForwardAddresses = utils.NetworkAddressListVar(nil)
+	// ReverseAddresses are ports that are allowed to be forwarded (in reverse)
+	ReverseAddresses = utils.NetworkAddressListVar(nil)
 
 	// HostKeyPath is the path to the host key
 	HostKeyPath = "hostkey.pem"
@@ -78,10 +78,10 @@ func init() {
 
 	flag.StringVar(&ContainerShell, "shell", ContainerShell, "Shell to execute within the container")
 
-	flag.BoolVar(&NoAuth, "unsafe", NoAuth, "Disable ssh server authentication and alllow anyone to connect")
+	flag.BoolVar(&DisableAuthentication, "unsafe", DisableAuthentication, "Disable ssh server authentication and alllow anyone to connect")
 
-	flag.Var(&ForwardPorts, "L", "Ports to allow local forwarding for")
-	flag.Var(&ReversePorts, "R", "Ports to allow reverse forwarding for")
+	flag.Var(&ForwardAddresses, "L", "Ports to allow local forwarding for")
+	flag.Var(&ReverseAddresses, "R", "Ports to allow reverse forwarding for")
 
 	flag.StringVar(&HostKeyPath, "hostkey", HostKeyPath, "Path to the host key")
 
